308b6e7e764f705be05560fa63004a957a864c49
Julia2906 <137899526+Julia2906@users.noreply.github.com>
2025-02-27 17:33:14 +0200

    css

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -2,7 +2,7 @@ import styles from './Description.module.css';
 
 const Description = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1 className={styles.title}>Sip Happens Café</h1>
       <p className={styles.description}>
         Please leave your feedback about our service by selecting one of the
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,11 +1,28 @@
+import styles from './Options.module.css';
+
 const Options = ({ feedback, onUpdateFeedback, totalfeedback }) => {
   return (
-    <div>
-      <button onClick={() => onUpdateFeedback('good')}> Good</button>
-      <button onClick={() => onUpdateFeedback('neutral')}>Neutral</button>
-      <button onClick={() => onUpdateFeedback('bad')}>Bad</button>
+    <div className={styles.container}>
+      <button
+        className={styles.btnGood}
+        onClick={() => onUpdateFeedback('good')}
+      >
+        {' '}
+        Good
+      </button>
+      <button
+        className={styles.btnNeutral}
+        onClick={() => onUpdateFeedback('neutral')}
+      >
+        Neutral
+      </button>
+      <button className={styles.btnBad} onClick={() => onUpdateFeedback('bad')}>
+        Bad
+      </button>
       {totalfeedback > 0 && (
-        <button onClick={() => onUpdateFeedback(0)}>Reset</button>
+        <button className={styles.btnReset} onClick={() => onUpdateFeedback(0)}>
+          Reset
+        </button>
       )}
     </div>
   );
